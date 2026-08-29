97a71e8be8415597c5264d7cb71aff5b56e93e1c
abbaj4n <arianofficial200@gmail.com>
2026-08-30 01:19:59 +0600

    Update index.js

--- src/handler/index.js
+++ src/handler/index.js
@@ -9,11 +9,11 @@ const globPromise = promisify(glob);
  * @param {Client} client
  */
 module.exports = async (client) => {
-    // Commands
+    // 1. Prefix Commands Loading
     const commandFiles = await globPromise(`${process.cwd()}/src/commands/**/*.js`);
-    commandFiles.map((value) => {
+    commandFiles.forEach((value) => {
         const file = require(value);
-        const splitted = value.split("/");
+        const splitted = value.split(/[\\/]/);
         const directory = splitted[splitted.length - 2];
 
         if (file.name) {
@@ -22,12 +22,41 @@ module.exports = async (client) => {
         }
     });
 
-    // Events
+    // 2. Events Loading
     const eventFiles = await globPromise(`${process.cwd()}/src/events/*.js`);
-    eventFiles.map((value) => require(value));
+    eventFiles.forEach((value) => require(value));
 
-   
-   
-  
-  
+    // 3. Slash Commands Loading & Registration
+    const arrayOfSlashCommands = [];
+    const slashCommandFiles = await globPromise(`${process.cwd()}/src/slashCommands/**/*.js`);
+    
+    slashCommandFiles.forEach((value) => {
+        const file = require(value);
+        if (file?.name) {
+            client.slashCommands.set(file.name, file);
+
+            if (["MESSAGE", "USER"].includes(file.type)) delete file.description;
+            arrayOfSlashCommands.push(file);
+        }
+    });
+
+    // 4. Auto Register Slash Commands on Ready
+    client.on("ready", async () => {
+        try {
+            await client.application.commands.set(arrayOfSlashCommands);
+            console.log("Successfully registered all Slash Commands globally!");
+        } catch (error) {
+            console.error("Failed to register Slash Commands:", error);
+        }
+    });
+
+    // 5. MongoDB Connection
+    const mongoUrl = client.config.mongo;
+    if (mongoUrl) {
+        mongoose.connect(mongoUrl).then(() => {
+            console.log("Connected to MongoDB database!");
+        }).catch((err) => {
+            console.error("MongoDB Connection Error:", err);
+        });
+    }
 };
